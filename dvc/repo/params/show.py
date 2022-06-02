0da8293d73e79a73269bddf4128094984d7e6e24
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-06-02 12:32:40 +0200

    api: Add `get_params`.
    Closes #6507
    
    Uses `repo.params.show` with custom error_handler and postprocess the outputs for more user-friendly structure.
    
    Extend `repo.params.show` to accept `stages` argument to cover the "params of current stage" use case.

--- dvc/repo/params/show.py
+++ dvc/repo/params/show.py
@@ -2,7 +2,15 @@ import logging
 import os
 from collections import defaultdict
 from copy import copy
-from typing import TYPE_CHECKING, Callable, Dict, List, Optional, Tuple
+from typing import (
+    TYPE_CHECKING,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Tuple,
+)
 
 from scmrepo.exceptions import SCMError
 
@@ -62,12 +70,15 @@ def _read_params(
     params_fs_paths,
     deps=False,
     onerror: Optional[Callable] = None,
+    stages: Optional[Iterable[str]] = None,
 ):
     res: Dict[str, Dict] = defaultdict(lambda: defaultdict(dict))
     fs_paths = copy(params_fs_paths)
 
     if deps:
         for param in params:
+            if stages and param.stage.name not in stages:
+                continue
             params_dict = error_handler(param.read_params)(
                 onerror=onerror, flatten=False
             )
@@ -75,7 +86,11 @@ def _read_params(
                 name = os.sep.join(repo.fs.path.relparts(param.fs_path))
                 res[name]["data"].update(params_dict["data"])
     else:
-        fs_paths += [param.fs_path for param in params]
+        fs_paths += [
+            param.fs_path
+            for param in params
+            if not stages or param.stage.name in stages
+        ]
 
     for fs_path in fs_paths:
         from_path = _read_fs_path(repo.fs, fs_path, onerror=onerror)
@@ -86,11 +101,13 @@ def _read_params(
     return res
 
 
-def _collect_vars(repo, params) -> Dict:
+def _collect_vars(repo, params, stages=None) -> Dict:
     vars_params: Dict[str, Dict] = defaultdict(dict)
 
     for stage in repo.index.stages:
         if isinstance(stage, PipelineStage) and stage.tracked_vars:
+            if stages and stage.name not in stages:
+                continue
             for file, vars_ in stage.tracked_vars.items():
                 # `params` file are shown regardless of `tracked` or not
                 # to reduce noise and duplication, they are skipped
@@ -103,14 +120,26 @@ def _collect_vars(repo, params) -> Dict:
 
 
 @locked
-def show(repo, revs=None, targets=None, deps=False, onerror: Callable = None):
+def show(
+    repo,
+    revs=None,
+    targets=None,
+    deps=False,
+    onerror: Callable = None,
+    stages=None,
+):
     if onerror is None:
         onerror = onerror_collect
     res = {}
 
     for branch in repo.brancher(revs=revs):
         params = error_handler(_gather_params)(
-            repo=repo, rev=branch, targets=targets, deps=deps, onerror=onerror
+            repo=repo,
+            rev=branch,
+            targets=targets,
+            deps=deps,
+            onerror=onerror,
+            stages=stages,
         )
 
         if params:
@@ -137,7 +166,9 @@ def show(repo, revs=None, targets=None, deps=False, onerror: Callable = None):
     return res
 
 
-def _gather_params(repo, rev, targets=None, deps=False, onerror=None):
+def _gather_params(
+    repo, rev, targets=None, deps=False, onerror=None, stages=None
+):
     param_outs, params_fs_paths = _collect_configs(repo, rev, targets=targets)
     params = _read_params(
         repo,
@@ -145,8 +176,9 @@ def _gather_params(repo, rev, targets=None, deps=False, onerror=None):
         params_fs_paths=params_fs_paths,
         deps=deps,
         onerror=onerror,
+        stages=stages,
     )
-    vars_params = _collect_vars(repo, params)
+    vars_params = _collect_vars(repo, params, stages=stages)
 
     # NOTE: only those that are not added as a ParamDependency are
     # included so we don't need to recursively merge them yet.
